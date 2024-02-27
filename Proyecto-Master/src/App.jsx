288a645babea2dac5d1543fee4a5fd703a3e1a8b
isapsanch <isabelpsanch@hotmail.com>
2024-02-27 18:31:23 +0100

    Añadir configuración en App.jsx

--- Proyecto-Master/src/App.jsx
+++ Proyecto-Master/src/App.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return(
-    <h1 className="text-3xl font-bold underline bg-red-200">
+    <h1 className="text-3xl font-bold underline">
       Hello World!!
+      <div className="bg-blue-300 flex justify-between"> 
+        <div className="bg-red-200 w-fit"> soy el hijo</div>
+        <div className="bg-red-200 w-fit"> soy el hijo</div>
+        <div className="bg-red-200 w-fit"> soy el hijo</div>
+      </div>
     </h1>
   )
 }
